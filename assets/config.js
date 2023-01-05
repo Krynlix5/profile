5e0603f636ab8feac9b8cf9320ba3d30a77b7de6
Krynlix <99536041+Krynlix5@users.noreply.github.com>
2023-01-05 15:53:19 +0100

    Actualizar config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 const config = {
     
-    USERNAME: "User#0000",
+    USERNAME: "Krynlix#6658",
     /**
      * This can either be a file link (relative or external)
      * or a Gravatar email or hash.
